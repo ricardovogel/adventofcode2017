3e2cabe70c64a4991d26e3a96b2e1970dfad5528
Ricardo Vogel <info@ricardovogel.nl>
2019-02-04 15:21:37 +0100

    Day 6

---
+++ day06/d06c1.py
@@ -0,0 +1,23 @@
+blocks = []
+with open("day06/input.txt") as f:
+    for line in f:
+        for word in line.split():
+            blocks.append(int(word))
+f.close()
+
+
+known = []
+while blocks not in known:
+    known.append(list(blocks))
+    maxValue = max(blocks)
+    maxIndex = blocks.index(maxValue)
+
+    blocks[maxIndex] = 0
+
+    index = maxIndex + 1
+    while maxValue > 0:
+        blocks[index % len(blocks)] += 1
+        index += 1
+        maxValue -= 1
+
+print(len(known))
